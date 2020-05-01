0f551bbc7ae10902872b5bfad7e5f00b81781020
jasutrb <43812984+jasutrb@users.noreply.github.com>
2020-05-01 23:28:20 +0900

    aaa

---
+++ recipi_finder/recipi_finder/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  recipi_finder
+//
+//  Created by 釣部正義 on 2020/05/01.
+//  Copyright © 2020 jasu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
