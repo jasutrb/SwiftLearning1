07e7624750cadb473454795e04c59da229a5fe62
jasutrb <43812984+jasutrb@users.noreply.github.com>
2020-05-02 14:22:01 +0900

    re

---
+++ GithubSearch/GithubSearch/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  GithubSearch
+//
+//  Created by 釣部正義 on 2018/12/31.
+//  Copyright © 2018年 jasu. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        configureView()
+    }
+
+    var detailItem: NSDate? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
